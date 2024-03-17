388c54a202320c2f20bffc0d579c771c272deee1
Stepan Gotko <40367054+sgotko@users.noreply.github.com>
2024-03-17 12:13:53 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import jest from 'eslint-plugin-jest';
+
+const config = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs['recommended-flat'],
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    files: ['**/*.ts', '**/*.d.ts', '**/*.tsx'],
+    ignores: ['dist/**/*.d.ts'],
+    rules: {
+      '@stylistic/semi': ['warn', 'always'],
+    },
+  },
+  {
+    ...tseslint.configs.disableTypeChecked,
+    files: ['**/*.js'],
+    rules: {
+      '@stylistic/semi': ['warn', 'always'],
+    },
+  },
+  {
+    ...eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...jest.configs['flat/recommended'],
+    ...jest.configs['flat/style'],
+    ...stylistic.configs['recommended-flat'],
+    files: ['test/**/*.test.ts'],
+  },
+);
+
+export default config;
---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { greetings } from '@/index.ts';
+
+describe('Simple test', () => {
+  it('Hello world', () => {
+    const greetingMessage = greetings('Bob');
+    expect(greetingMessage).toBe('Hello, Bob!');
+  });
+});
